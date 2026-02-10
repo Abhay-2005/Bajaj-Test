df81400caf561ddaa42c4201a4a1c003276dc209
Abhay-2005 <abhay1015.be23@chitkarauniversity.edu.in>
2026-02-10 13:05:17 +0530

    Fix package.json for deployment

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const EMAIL = process.env.EMAIL;
 
 console.log("Gemini Key:", process.env.GEMINI_KEY);
